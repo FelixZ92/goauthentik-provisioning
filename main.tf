07723375404218e5c87e8467f300d89708e76437
Felix Zippel <contact@zippelf.com>
2023-09-24 13:50:23 +0200

    Test var

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
+variable "test_var" {
+  default = ""
+}
+
 output "my_ip_addr" {
   value = data.http.myip.body
+}
+
+output "wusa" {
+  value = var.test_var
 }
