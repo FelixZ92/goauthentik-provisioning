4ab4b9a6828a448e3933dd7fd76c47f6c56a6550
Felix Zippel <contact@zippelf.com>
2023-09-24 16:07:25 +0200

    Add github oauth source

---
+++ github.tf
@@ -0,0 +1,17 @@
+data "authentik_flow" "default-authentication-flow" {
+  slug = "default-authentication-flow"
+}
+
+data "authentik_flow" "default-enrollment-flow" {
+  slug = "default-source-enrollment"
+}
+
+resource "authentik_source_oauth" "github_app" {
+  authentication_flow = data.authentik_flow.default-authentication-flow.id
+  consumer_key        = var.github_app_client_secret
+  consumer_secret     = var.github_app_client_secret
+  enrollment_flow     = data.authentik_flow.default-enrollment-flow
+  name                = "Github"
+  provider_type       = "github"
+  slug                = "github"
+}
--- vars.tf
+++ vars.tf
@@ -19,3 +19,13 @@ variable "grafana_client_secret" {
   default = ""
   sensitive = true
 }
+
+variable "github_app_client_secret" {
+  default = ""
+  sensitive = true
+}
+
+variable "github_app_client_id" {
+  default = ""
+  sensitive = true
+}
