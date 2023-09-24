1ee4d497e9c3ed6b60068e084507d015e6c77523
Felix Zippel <contact@zippelf.com>
2023-09-24 20:47:09 +0200

    Add read:org to scopes

--- github.tf
+++ github.tf
@@ -11,6 +11,7 @@ resource "authentik_source_oauth" "github_app" {
   consumer_key        = var.github_app_client_id
   consumer_secret     = var.github_app_client_secret
   enrollment_flow     = data.authentik_flow.default-enrollment-flow.id
+  additional_scopes = "read:org"
   name                = "Github"
   provider_type       = "github"
   slug                = "github"
