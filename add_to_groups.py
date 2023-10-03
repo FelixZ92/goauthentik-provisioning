d5f0f743623d8fb0a893b279a1d1c2eab9643612
Felix Zippel <contact@zippelf.com>
2023-10-03 19:54:01 +0200

    Update enrollment flow

--- add_to_groups.py
+++ add_to_groups.py
@@ -1,5 +1,5 @@
 from authentik.core.models import Group
-email = request.context["pending_user"]["email"]
+email = request.context["prompt_data"]["email"]
 group1, _ = Group.objects.get_or_create(name="some-group")
 group2, _ = Group.objects.get_or_create(name=email)
 # ["groups"] *must* be set to an array of Group objects, names alone are not enough.
