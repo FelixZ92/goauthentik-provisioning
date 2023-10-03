d2e74847a4fee1fe4eee3b78464c68e956382fc1
Felix Zippel <contact@zippelf.com>
2023-10-03 20:14:47 +0200

    fix policy

--- add_to_groups.py
+++ add_to_groups.py
@@ -7,6 +7,5 @@ if email == "contact@zippelf.com" and username.lower() == "felixz92":
     cluster_admins, _ = Group.objects.get_or_create(name="cluster-admins")
     wego_admins, _ = Group.objects.get_or_create(name="wego-admin")
     wego_readonly, _ = Group.objects.get_or_create(name="wego-readonly")
-# ["groups"] *must* be set to an array of Group objects, names alone are not enough.
-request.context["flow_plan"].context["groups"] = [grafana_editors, grafana_admins, cluster_admins, wego_admins, wego_readonly]
+    request.context["flow_plan"].context["groups"] = [grafana_editors, grafana_admins, cluster_admins, wego_admins, wego_readonly]
 return True
