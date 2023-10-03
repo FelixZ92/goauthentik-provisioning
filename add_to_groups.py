25387f9aec4adab34f05ff12b9080bca223e6eb2
Felix Zippel <contact@zippelf.com>
2023-10-03 20:06:16 +0200

    Update enrollment flow

--- add_to_groups.py
+++ add_to_groups.py
@@ -1,7 +1,12 @@
 from authentik.core.models import Group
 email = request.context["prompt_data"]["email"]
-group1, _ = Group.objects.get_or_create(name="some-group")
-group2, _ = Group.objects.get_or_create(name=email)
+username = request.context["oauth_userinfo"]["login"]
+if email == "contact@zippelf.com" and username.lower() == "felixz92":
+    grafana_admins, _ = Group.objects.get_or_create(name="Grafana Admins")
+    grafana_editors, _ = Group.objects.get_or_create(name="Grafana Editors")
+    cluster_admins, _ = Group.objects.get_or_create(name="cluster-admins")
+    wego_admins, _ = Group.objects.get_or_create(name="wego-admin")
+    wego_readonly, _ = Group.objects.get_or_create(name="wego-readonly")
 # ["groups"] *must* be set to an array of Group objects, names alone are not enough.
-request.context["flow_plan"].context["groups"] = [group1, group2]
+request.context["flow_plan"].context["groups"] = [grafana_editors, grafana_admins, cluster_admins, wego_admins, wego_readonly]
 return True
